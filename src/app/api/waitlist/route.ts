18e07342932955c4f610b9d3c554fc98e90893d5
sam <shenqjye93@gmail.com>
2026-09-02 15:26:57 +0800

    fix: update google auth

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -33,8 +33,11 @@ export async function POST(request: NextRequest) {
 		const auth = new google.auth.GoogleAuth({
 			credentials: {
 				type: "service_account",
+				project_id: process.env.GOOGLE_PROJECT_ID,
+				private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
 				private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-				client_email: process.env.GOOGLE_CLIENT_EMAIL,
+				client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+				client_id: process.env.GOOGLE_CLIENT_ID,
 			} as Record<string, string | undefined>,
 			scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 		});
